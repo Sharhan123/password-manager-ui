5ddf71a17ce7cf9bd1101a1be89613e7a65f6745
Sharhan123 <sharhanmohammed03@gmail.com>
2024-07-03 16:36:57 +0530

    last commit

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosWithAuth = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "password-manager.electromania.tech/api",
 });
 
 axiosWithAuth.interceptors.request.use(
